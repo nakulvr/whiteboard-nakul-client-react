b898945c7efd9ee58a7ab2678bb939ae81cf9bf5
vrnak <vrnakul@gmail.com>
2018-10-31 00:05:46 -0400

    editing URL

--- src/services/CourseServiceSingleton.js
+++ src/services/CourseServiceSingleton.js
@@ -1,5 +1,5 @@
 // let courses = fetch("../courses.json")
-//     .then(response => response.json());
+//     .then(response => response.json());;
 let courses = require('../courses.json');
 // let COURSE_API_URL = "http://localhost:8080/api/user/";
 let COURSE_API_URL ="https://whiteboard-nakul-server-java.herokuapp.com/api/user/";
